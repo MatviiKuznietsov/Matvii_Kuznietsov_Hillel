ae3befefecb67f41484c897e147b01918da69361
Matvii <matveimtvcool@gmail.com>
2022-11-10 20:43:29 +0200

    Add void to Logger class

--- src/ua/hillel/messenger/Logger.java
+++ src/ua/hillel/messenger/Logger.java
@@ -1,25 +1,19 @@
 package ua.hillel.messenger;
 
 public class Logger {
-    static String info(String status) {
+    static void info(String status) {
         System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "Status - " + status);
-        return "NULL";
     }
 
-    static String debug(String status) {
+    static void debug(String status) {
         System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT + "Status - " + status);
-        return "NULL";
     }
 
-    static String warn(String status) {
+    static void warn(String status) {
         System.out.println(ConsoleColors.PURPLE_BOLD_BRIGHT + "Status - " + status);
-        return "NULL";
     }
 
-    static String error(String status) {
+    static void error(String status) {
         System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Status - " + status);
-        return "NULL";
     }
-
-
 }
