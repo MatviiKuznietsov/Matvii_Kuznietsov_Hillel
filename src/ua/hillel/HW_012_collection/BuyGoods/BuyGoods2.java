0f6af1d552dfeb6b3258f24efaf967a7eb30eb6f
Matvii <113104498+MatviiKuznietsov@users.noreply.github.com>
2022-11-28 18:33:43 +0200

    Delete BuyGoods2.java

--- src/ua/hillel/HW_012_collection/BuyGoods/BuyGoods2.java
+++
@@ -1,9 +0,0 @@
-package ua.hillel.HW_012_collection.BuyGoods;
-
-public class BuyGoods2 {
-    public static void main(String[] args) {
-        CollectionMtethods collectionMtethods = new CollectionMtethods();
-        collectionMtethods.fillGoods();
-        collectionMtethods.summQuntityGoods();
-    }
-}
