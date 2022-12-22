b4f14c0df625a384f01788112656df05086abeae
Matvii <matveimtvcool@gmail.com>
2022-12-22 10:41:38 +0200

    Drag and Drope test Done

--- src/test/java/ua/hillel/tests/PageTestAction.java
+++ src/test/java/ua/hillel/tests/PageTestAction.java
@@ -1,14 +1,24 @@
 package ua.hillel.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 
 public class PageTestAction extends BaseTest {
     @Test
     public void dragElements() {
         driver.get("https://crossbrowsertesting.github.io/drag-and-drop.html");
+        WebElement dragFrame = driver.findElement(By.xpath("//div[@class='ui-widget-content ui-draggable ui-draggable-handle']"));
+        WebElement enterFrame = driver.findElement(By.xpath("//*[@id='droppable']"));
+        Actions action = new Actions(driver);
+        action.dragAndDrop(dragFrame,enterFrame).perform();
     }
+
 
     @Test
     public void dropDownMenu() {
