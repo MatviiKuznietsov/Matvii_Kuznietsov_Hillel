9a4cfcebecaa31732ad065ff7eee0f73f45036e5
Matvii <matveimtvcool@gmail.com>
2022-12-22 11:23:34 +0200

    Drope done menu test Done

--- src/test/java/ua/hillel/tests/PageTestAction.java
+++ src/test/java/ua/hillel/tests/PageTestAction.java
@@ -1,13 +1,21 @@
 package ua.hillel.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
 
 public class PageTestAction extends BaseTest {
     @Test
@@ -16,14 +24,28 @@ public class PageTestAction extends BaseTest {
         WebElement dragFrame = driver.findElement(By.xpath("//div[@class='ui-widget-content ui-draggable ui-draggable-handle']"));
         WebElement enterFrame = driver.findElement(By.xpath("//*[@id='droppable']"));
         Actions action = new Actions(driver);
-        action.dragAndDrop(dragFrame,enterFrame).perform();
+        action.dragAndDrop(dragFrame, enterFrame).pause(3000).perform();
     }
 
 
     @Test
     public void dropDownMenu() {
         driver.get("https://crossbrowsertesting.github.io/hover-menu.html");
+        WebElement dropDown = driver.findElement(By.cssSelector(".dropdown-toggle"));
+        WebElement secondaryMenu = driver.findElement(By.xpath(".//*[text()='Secondary Menu ']"));
+        WebElement secondaryAction = driver.findElement(By.xpath(".//*[text()='Secondary Action']"));
+        // Select select = new Select(dropDown);
+        // select.selectByVisibleText("Another action");
+        Actions actions = new Actions(driver);
+        actions.moveToElement(dropDown)
+                .pause(1000)
+                .moveToElement(secondaryMenu)
+                .pause(1000)
+                .click(secondaryAction)
+                .perform();
+        Assert.assertEquals(driver.findElement(By.xpath(".//*[text()='Secondary Page']")).getText(),"Secondary Page");
     }
+
 
     @Test
     public void userName() {
